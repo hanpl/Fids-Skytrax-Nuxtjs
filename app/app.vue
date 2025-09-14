4cc830ada72930205994f1a05012218d65d2e982
phamlahan <39558227+hanpl@users.noreply.github.com>
2025-09-14 11:18:55 +0700

    1409

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav class="p-4 bg-gray-200 flex gap-4">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/contact">Contact</NuxtLink>
+    </nav>
+    <NuxtPage />
+  </div>
+</template>
